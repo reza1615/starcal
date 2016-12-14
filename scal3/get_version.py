988c6795b0072366315f6d274d2bbf6b9776abee
Saeed Rasooli <saeed.gnu@gmail.com>
2016-12-15 01:46:00 +0330

    replace indentation 4 spaces with tabs in all .py files (using my bash script)

--- scal3/get_version.py
+++ scal3/get_version.py
@@ -5,9 +5,9 @@ VERSION = ''
 
 fp = open('%s/core.py'%os.path.dirname(__file__))
 while True:
-    line = fp.readline()
-    if line.startswith('VERSION'):
-        exec(line)
-        break
+	line = fp.readline()
+	if line.startswith('VERSION'):
+		exec(line)
+		break
 print(VERSION)
 
